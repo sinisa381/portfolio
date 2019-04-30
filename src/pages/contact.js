29c4d82837b9d447c3b9f24956c0d71681f909e1
Sinisa Colic <sinisa820@gmail.com>
2019-04-30 15:22:18 +0200

    form gatsby

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,14 +5,30 @@ import { Container } from '../components/shared'
 export default ({ location }) => (
 	<Layout location={location}>
 		<Container>
-			<form type='POST' data-netlify='true'>
-				<input type='text' name='name' id='name' placeholder='name' />
-				<input type='email' name='email' id='email' placeholder='email' />
-				<textarea name='message' id='message' cols='30' rows='10'>
-					Enter message
-				</textarea>
-				<input type='submit' value='Send message' />
-				<div data-netlify-recaptcha='true' />
+			<form method='POST' action='#'>
+				<input type='hidden' name='bot-field' />
+				<input type='hidden' name='form-name' value='contact' />
+				<input type='hidden' name='form-name' value='contact' />
+				<div className='field half first'>
+					<label htmlFor='name'>Name</label>
+					<input type='text' name='name' id='name' />
+				</div>
+				<div className='field half'>
+					<label htmlFor='email'>Email</label>
+					<input type='text' name='email' id='email' />
+				</div>
+				<div className='field'>
+					<label htmlFor='message'>Message</label>
+					<textarea name='message' id='message' rows='6' />
+				</div>
+				<ul className='actions'>
+					<li>
+						<input type='submit' value='Send Message' className='special' />
+					</li>
+					<li>
+						<input type='reset' value='Clear' />
+					</li>
+				</ul>
 			</form>
 		</Container>
 	</Layout>
